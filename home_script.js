4b96fb4fad2c8fd4c30753fb9229a76925ffc705
Sourin <iamsourinsarkar@gmail.com>
2021-09-07 01:08:54 +0530

    Added the dropdown for hamburger menu

--- home_script.js
+++ home_script.js
@@ -2,3 +2,7 @@
 function home_box1() {
     window.location.href='https://sourin.in';    
 }
+
+function dropdownSection() {
+    document.getElementById('dropdownSection').style.display = "block";
+ }
