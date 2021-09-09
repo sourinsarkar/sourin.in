7b00cd3fe1807f84ff425ddc00f21603e70034a3
Sourin <iamsourinsarkar@gmail.com>
2021-09-09 12:09:07 +0530

    Added files

---
+++ home_script.js
@@ -0,0 +1,8 @@
+// FOR BOX-1
+function home_box1() {
+    window.location.href='https://sourin.in';    
+}
+
+function dropdownSection() {
+    document.getElementById('dropdownSection').style.display = "block";
+ }
